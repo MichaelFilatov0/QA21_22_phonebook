bf6618e01c813a1a612d4cd7aa452ad1eafc83c2
MichaelFilatov0 <118763960+MichaelFilatov0@users.noreply.github.com>
2023-12-13 21:12:18 +0200

    loginTests

--- QA21_22_Phonebook/src/main/java/manager/HelperUser.java
+++ QA21_22_Phonebook/src/main/java/manager/HelperUser.java
@@ -1,9 +1,32 @@
 package manager;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class HelperUser extends HelperBase{
     public HelperUser(WebDriver wd) {
         super(wd);
+    }
+    public void openLoginRegistrationForm(){
+        WebElement loginTab =wd.findElement(By.cssSelector("a[href='/login']"));
+        loginTab.click();
+    }
+    public void fillLoginRegistrationForm(String email, String password){
+//        WebElement emailinput = wd.findElement(By.name("email"));
+//        emailinput.click();
+//        emailinput.clear();
+//        emailinput.sendKeys(email);
+//
+//        WebElement passwordinput= wd.findElement(By.xpath("//input[last()]"));
+        type(By.name("email"),email);
+        type(By.xpath("//input[last()]"),password);
+
+    }
+    public void submitLogin(){
+        click(By.name("login"));
+    }
+    public boolean isLogged(){
+        return isElementPresent(By.xpath("//button[text()='Sign Out']"));
     }
 }
--- QA21_22_Phonebook/src/test/java/tests/LoginTests.java
+++ QA21_22_Phonebook/src/test/java/tests/LoginTests.java
@@ -1,10 +1,14 @@
 package tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class LoginTests extends TestBase{
     @Test
     public void test(){
-
+      app.getHelperUser().openLoginRegistrationForm();
+      app.getHelperUser().fillLoginRegistrationForm("a@2.com","Mm@2756808");
+      app.getHelperUser().submitLogin();
+        Assert.assertTrue(app.getHelperUser().isLogged());
     }
 }
